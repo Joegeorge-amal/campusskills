8fd4d6380a6f6471f2804f0652f85eaf08cf4630
Joegeorge-amal <amaljogeorge@gmail.com>
2026-06-03 14:56:06 +0530

    delete

--- frontend/README.md
+++
@@ -1,4 +0,0 @@
-# CampusSkills Frontend
-
-This folder is reserved for the future Angular integration. 
-The Angular application will communicate with the Vert.x backend via REST APIs and WebSockets.
--- frontend/src/services/profileService.js
+++
@@ -1,7 +0,0 @@
-import api from './api';
-
-export const profileService = {
-  getMe: () => api.get('/profiles/me'),
-  updateMe: (data) => api.patch('/profiles/me', data),
-  getProfile: (userId) => api.get(`/profiles/${userId}`)
-};
