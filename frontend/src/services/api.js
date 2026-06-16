4fa52c487157b70166222348e3300e640c8e2b24
Joegeorge-amal <amaljogeorge@gmail.com>
2026-06-16 18:55:04 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create central API client pointing to your Java Vert.x back-end relative API endpoint
 const api = axios.create({
-  baseURL: '/api/v1',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json'
   },
